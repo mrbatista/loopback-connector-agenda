e701660de33207dbc4c6f44d0b527821f8254b27
Matteo Padovano <mrba7ista@gmail.com>
2017-09-21 15:25:43 +0200

    convert all mixin methods to dual callback/promise API

--- test/index.test.js
+++ test/index.test.js
@@ -1,27 +1,25 @@
 'use strict';
 
 var loopback = require('loopback');
-var chai = require('chai');
-chai.use(require('dirty-chai'));
-var expect = chai.expect;
+var expect = require('./expect');
 var AgendaConnector = require('../');
 var app = loopback();
 
 describe('Loopback Agenda Connector', function() {
   var QueueJob, ds;
 
-  before(function(done) {
+  before(function() {
     ds = loopback.createDataSource({connector: AgendaConnector});
     QueueJob = app.model('QueueJob', {dataSource: ds, public: false});
-    QueueJob.delete(done);
+    return QueueJob.delete();
   });
 
-  afterEach(function(done) {
-    QueueJob.delete(done);
+  afterEach(function() {
+    return QueueJob.delete();
   });
 
-  after(function(done) {
-    ds.disconnect(done);
+  after(function() {
+    return ds.disconnect();
   });
 
   it('Create job to be run now', function(done) {
@@ -33,12 +31,28 @@ describe('Loopback Agenda Connector', function() {
     });
   });
 
-  it('Create scheduled job', function(done) {
-      QueueJob.schedule(new Date(), 'test', function(err, job) {
-        if (err) return done(err);
+  it('Create job to be run now - promise variant', function() {
+    return QueueJob.now('test-email')
+      .then(function(job) {
         expect(job).to.exist();
         expect(job.attrs._id).to.exist();
-        done();
+      });
+  });
+
+  it('Create scheduled job', function(done) {
+    QueueJob.schedule(new Date(), 'test', function(err, job) {
+      if (err) return done(err);
+      expect(job).to.exist();
+      expect(job.attrs._id).to.exist();
+      done();
+    });
+  });
+
+  it('Create scheduled job - promise variant', function() {
+    return QueueJob.schedule(new Date(), 'test')
+      .then(function(job) {
+        expect(job).to.exist();
+        expect(job.attrs._id).to.exist();
       });
   });
 
@@ -49,14 +63,44 @@ describe('Loopback Agenda Connector', function() {
       doneJob();
       done();
     });
-    
+
     QueueJob.every("1 seconds", 'test-rec', function(err, job) {
       if (err) return done(err);
       expect(job).to.exist();
       expect(job.attrs._id).to.exist();
-      });
+    });
   });
-  
+
+  it('Create recurrent job - promise variant', function(done) {
+    QueueJob.define('test-rec')
+      .spread(function(j, doneJob) {
+        expect(j).to.exist();
+        expect(j.attrs._id).to.exist();
+        doneJob();
+        done();
+      });
+
+    QueueJob.every("1 seconds", 'test-rec')
+      .then(function(job) {
+        expect(job).to.exist();
+        expect(job.attrs._id).to.exist();
+      }).catch(done);
+  });
+
+  it('Create recurrent job without done job callback', function(done) {
+    QueueJob.define('test-rec', function(j) {
+      expect(j).to.exist();
+      expect(j.attrs._id).to.exist();
+      done();
+    });
+
+    QueueJob.every("1 seconds", 'test-rec', function(err, job) {
+      if (err) return done(err);
+      expect(job).to.exist();
+      expect(job.attrs._id).to.exist();
+    });
+  });
+
   it('Define job', function(done) {
     var job;
     QueueJob.define('test', function(j, doneJob) {
@@ -71,6 +115,86 @@ describe('Loopback Agenda Connector', function() {
       expect(job).to.exist();
       expect(job.attrs._id).to.exist();
     });
+  });
+
+  it('Define job - promise variant', function(done) {
+    var job;
+    QueueJob.define('test')
+      .spread(function(j, doneJob) {
+        expect(j.attrs._id).to.be.eql(job.attrs._id);
+        doneJob();
+        done();
+      });
+
+    QueueJob.schedule(new Date(), 'test')
+      .then(function(j) {
+        job = j;
+        expect(job).to.exist();
+        expect(job.attrs._id).to.exist();
+      }).catch(done);
+  });
+
+  it('Define job that fail', function(done) {
+    var job;
+    QueueJob.define('test-fail', function(j, doneJob) {
+      var jobId = j.attrs._id;
+      expect(jobId).to.be.eql(job.attrs._id);
+      var error = new Error('generic error');
+      doneJob(error);
+      setTimeout(function() {
+        QueueJob.find({_id: jobId}, function(err, jobs) {
+          if (err) return done(err);
+          expect(jobs).to.be.instanceof(Array);
+          expect(jobs.length).to.be.equal(1);
+          var job2 = jobs[0];
+          var attrs = job2.attrs;
+          expect(attrs).to.exist();
+          expect(attrs.failedAt).to.be.instanceof(Date);
+          expect(attrs.failReason).to.be.equal('generic error');
+          expect(attrs.failCount).to.be.equal(1);
+          done();
+        });
+      }, 200);
+    });
+
+    QueueJob.now('test-fail', function(err, j) {
+      if (err) return done(err);
+      job = j;
+      expect(job).to.exist();
+      expect(job.attrs._id).to.exist();
+    });
+  });
+
+  it('Define job that fail - promise variant', function(done) {
+    var job;
+    QueueJob.define('test-fail')
+      .spread(function(j, doneJob) {
+        var jobId = j.attrs._id;
+        expect(jobId).to.be.eql(job.attrs._id);
+        var error = new Error('generic error');
+        doneJob(error);
+        setTimeout(function() {
+          QueueJob.find({_id: jobId})
+            .then(function(jobs) {
+              expect(jobs).to.be.instanceof(Array);
+              expect(jobs.length).to.be.equal(1);
+              var job2 = jobs[0];
+              var attrs = job2.attrs;
+              expect(attrs).to.exist();
+              expect(attrs.failedAt).to.be.instanceof(Date);
+              expect(attrs.failReason).to.be.equal('generic error');
+              expect(attrs.failCount).to.be.equal(1);
+              done();
+            }).catch(done);
+        }, 200);
+      });
+
+    QueueJob.now('test-fail')
+      .then(function(j) {
+        job = j;
+        expect(job).to.exist();
+        expect(job.attrs._id).to.exist();
+      }).catch(done);
   });
 
   it('Find specific job', function(done) {
@@ -88,4 +212,16 @@ describe('Loopback Agenda Connector', function() {
     });
   });
 
+  it('Find specific job - promise variant', function() {
+    return QueueJob.schedule(new Date(), 'test', {text: 'Ciao'})
+      .then(function(job) {
+        expect(job).to.exist();
+        expect(job.attrs._id).to.exist();
+        return QueueJob.find({'data.text': 'Ciao'}).then(function(jobs) {
+          expect(jobs).to.be.instanceOf(Array);
+          expect(jobs.length).to.be.equal(1);
+          expect(jobs[0].attrs.data.text).to.be.equal('Ciao');
+        })
+      });
+  });
 });
